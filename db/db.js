2c8f2dd48ab0685ab33126a622607876700eaf44
Roene <roene.verbeek@hva.nl>
2020-03-22 12:04:13 +0100

    Connection to the database can be maked now & installed Mongoose

---
+++ db/db.js
@@ -0,0 +1,8 @@
+// THIS FILE MAKES A DATABASE CONNECTION
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const uri = process.env.MONGODB_URI
+const dbconnection = () => mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected...') )
+
+module.exports = dbconnection
