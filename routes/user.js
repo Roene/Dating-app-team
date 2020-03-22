bf055330053382310b8932c13753db94d878a757
Roene <roene.verbeek@hva.nl>
2020-03-22 12:39:49 +0100

    Added get method to visit signup page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router    = express.Router()
 
 router
     .get('/', (req, res) => {res.render('pages/index')})
+    .get('/signup', (req, res) => {res.render('pages/signup')})
 
 
 module.exports = router
