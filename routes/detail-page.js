b4bafbc6902fb47c203a432b2a738422cfcb7ed3
Khualu <andrespint@gmail.com>
2020-04-10 12:47:10 +0200

    Cleaned up code in detail-page.js

--- routes/detail-page.js
+++ routes/detail-page.js
@@ -20,26 +20,8 @@ const upload = multer({ storage: storage })
 
 router
     .get('/match-profile/:_id', auth, (req, res) => {
-        // MOET MOGELIJK NOG GECHECKT WORDEN
+
         const rawId = req.params._id
-        // const searchId = rawId.substr(2)
-        // console.log(searchId)
-        
-        // User.findOne({ _id : rawId }, (err, foundObject) => {
-        //     if (err) {
-        //         console.log(err)
-        //         res.status(500).send()
-        //     } else {
-        //         if (!foundObject) {
-        //             res.status(404).send('User not found')
-        //             console.log('User could not be found')
-        //         } else {
-        //             const renderData = foundObject 
-        //             console.log(renderData)
-        //             res.render('pages/match-profile', { renderData, dataTop100 })
-        //         }
-        //     }
-        // })
 
         User.findOne({ _id : rawId }, (err, foundObject) => {
             if (err) {
@@ -59,27 +41,3 @@ router
     })
 
 module.exports = router
-
-// const name = req.body.userName
-//     console.log(name)
-
-//     gamesModel.findOne({ userName: name }, (err, foundObject) => {
-//       if (err) {
-//         console.log(err)
-//         res.status(500).send()
-//       } else {
-//         if (!foundObject) {
-//           res.status(404).send('404, User: ' + name + ' not found')
-//           console.log('Could not find user')
-//         } else {
-//           if (req.body.name) {
-//             foundObject.name = req.body.name
-//           }
-//           //   ZORGEN DAT GAME RENDER PLAATS VIND
-//           const userData = foundObject
-//           console.log(userData)
-//           res.render('pages/profile', { userData, top100Games })
-//         }
-//       }
-//     })
-//   })
