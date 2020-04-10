095d78bec7acd92785fa274c6269490ff1ca8b2e
Khualu <andrespint@gmail.com>
2020-04-10 12:44:29 +0200

    Made try/catch out of if/else statement

--- routes/detail-page.js
+++ routes/detail-page.js
@@ -25,22 +25,37 @@ router
         // const searchId = rawId.substr(2)
         // console.log(searchId)
         
+        // User.findOne({ _id : rawId }, (err, foundObject) => {
+        //     if (err) {
+        //         console.log(err)
+        //         res.status(500).send()
+        //     } else {
+        //         if (!foundObject) {
+        //             res.status(404).send('User not found')
+        //             console.log('User could not be found')
+        //         } else {
+        //             const renderData = foundObject 
+        //             console.log(renderData)
+        //             res.render('pages/match-profile', { renderData, dataTop100 })
+        //         }
+        //     }
+        // })
+
         User.findOne({ _id : rawId }, (err, foundObject) => {
             if (err) {
                 console.log(err)
                 res.status(500).send()
             } else {
-                if (!foundObject) {
-                    res.status(404).send('User not found')
-                    console.log('User could not be found')
-                } else {
+                try {
                     const renderData = foundObject 
                     console.log(renderData)
                     res.render('pages/match-profile', { renderData, dataTop100 })
+                } catch (err) {
+                    res.status(404).send('User not found')
+                    console.log(err)
                 }
             }
         })
-        
     })
 
 module.exports = router
